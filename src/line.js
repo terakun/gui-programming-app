30b4fd703798143b7f86662ccd75769226b16341
Takeru Serikawa <takeru.serikawa@gmail.com>
2019-05-17 03:19:53 +0900

    implement readability component

--- src/line.js
+++ src/line.js
@@ -9,6 +9,7 @@ export default class Line extends React.Component {
         const cy = ((y1 + y2) / 2) - (thickness / 2);
         const angle = Math.atan2((y1 - y2), (x1 - x2)) * (180 / Math.PI);
 
+
         const style = {
             padding: 0,
             margin: 0,
@@ -19,6 +20,7 @@ export default class Line extends React.Component {
             left: cx,
             top: cy,
             width: length,
+            height: 5,
             MozTransform: `rotate(${angle}deg)`,
             WebkitTransform: `rotate(${angle}deg)`,
             OTransform: `rotate(${angle}deg)`,
