146129c684e66ef3fb1a67fd6bcd373c5852f446
Terasaki Shun <t.tera1217@gmail.com>
2019-05-18 07:17:16 +0900

    Updates

---
+++ precache-manifest.215440cba0064df0df17f1cffb3666ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "670028f8ee9fe171a15f",
+    "url": "/gui-programming-app/static/js/runtime~main.79d1626b.js"
+  },
+  {
+    "revision": "eae78c2f515d347a95e2",
+    "url": "/gui-programming-app/static/js/main.a9b1b427.chunk.js"
+  },
+  {
+    "revision": "2910f2999d4989417ee9",
+    "url": "/gui-programming-app/static/js/2.1ebe3d11.chunk.js"
+  },
+  {
+    "revision": "3e1beeebd10ea178e4c9f5352d944c55",
+    "url": "/gui-programming-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/gui-programming-app/precache-manifest.215440cba0064df0df17f1cffb3666ba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/gui-programming-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
